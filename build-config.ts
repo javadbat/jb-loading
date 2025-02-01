c649a98a724e9f988cd304a618c18658398cb1b8
javadbat <javadbat@gmail.com>
2025-02-01 15:37:09 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,24 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-loading",
+    path: "./lib/JBLoading.js",
+    outputPath: "./dist/JBLoading.js",
+    external: [],
+    umdName: "JBLoading",
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-loading-react",
+    path: "./react/lib/JBLoading.tsx",
+    outputPath: "./react/dist/JBLoading.js",
+    external: ["jb-loading", "prop-types", "react"],
+    globals: {
+      "jb-loading": "JBLoading",
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
