335e6a9fa5e1dc40d513c3ec75dbef08e3355982
javadbat <javadbat@gmail.com>
2025-02-03 18:04:02 +0330

    chore: add dire & umdName to build config

--- build-config.ts
+++ build-config.ts
@@ -20,5 +20,7 @@ export const reactComponentList: ReactComponentBuildConfig[] = [
       react: "React",
       "prop-types": "PropTypes",
     },
+    umdName:"JBLoadingReact",
+    dir:"./react"
   },
 ];
