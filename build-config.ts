12d91650e07d6ddab090d18f4f51f587dd2c0aae
javadbat <javadbat@gmail.com>
2026-09-14 01:30:06 +0330

    chore: update core/i18n config

--- build-config.ts
+++ build-config.ts
@@ -10,7 +10,7 @@ export const webComponentList: WebComponentBuildConfig[] = [
     globals: {
       "jb-core": "JBCore",
       "jb-core/theme": "JBCoreTheme",
-      "jb-core/i18n": "JBCoreI18N",
+      "jb-core/i18n": "JBCoreI18n",
     },
     umdName: "JBLoading",
   },
